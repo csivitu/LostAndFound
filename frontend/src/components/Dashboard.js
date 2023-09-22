86557cf9a64f237a701c5eecbd322de62b62b11e
Priyanshu Kumar <priyanshukumar10714@gmail.com>
2023-09-22 21:52:49 +0530

    fix: fixed aspect ratio problem

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,12 +8,32 @@ export default function Dashboard() {
     { id: 2, name: "Victor Wayne" },
     { id: 3, name: "Jane Doe" },
   ];
+
+  // CSS styles as a string
+  const styles = `
+    .image-container {
+      position: relative;
+      overflow: hidden;
+      padding-top: 75%; /* 4:3 Aspect Ratio (You can adjust this value) */
+    }
+
+    .image-container img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  `;
+
   return (
     <>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="bg-light">
+        <style>{styles}</style> {/* Include the CSS styles here */}
         <Navbar />
         <br />
         <br />
@@ -38,21 +58,21 @@ export default function Dashboard() {
           <br />
           <div className="row">
             {data.map((user) => (
-              <div className={`col-sm-4`}>
+              <div className={`col-sm-4`} key={user.id}>
                 <div className="card shadow my-2 p-3">
-                  <img
-                    className="card-img-top img-thumbnail"
-                    src={`images/${user.id}.jpg`}
-                    alt="Image"
-                    style={{ width: "100%", height: "20rem" }}
-                  />
+                  <div className="image-container">
+                    <img
+                      className="card-img-top img-thumbnail"
+                      src={`images/${user.id}.jpg`}
+                      alt="Image"
+                    />
+                  </div>
                   <div className="card-body">
                     <h5 className="card-title">{user.name}</h5>
                     <p className="card-text">
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
                     </p>
-                    {/*<span className="btn btn-secondary">Go somewhere</span>*/}
                   </div>
                 </div>
               </div>
