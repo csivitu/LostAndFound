3e94705c9a35654de699314847995c7eb48a3b2c
gautam-4 <gautamalt468@gmail.com>
2023-09-23 12:39:52 +0530

    fuzzy search

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Fuse from "fuse.js"; // Import Fuse
 
 export default function Dashboard() {
-  const data = [
+  const initialData = [
     { id: 1, name: "John Doe" },
     { id: 2, name: "Victor Wayne" },
     { id: 3, name: "Jane Doe" },
   ];
+
+  const [data, setData] = useState(initialData);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const options = {
+    keys: ["name"], // Specify the key(s) to search within
+    threshold: 0.3, // Adjust the threshold for fuzzy matching
+  };
+
+  const fuse = new Fuse(initialData, options);
+
+  useEffect(() => {
+    if (searchTerm.trim() === "") {
+      // Reset data when the search input is empty
+      setData(initialData);
+    } else {
+      // Use Fuse to perform fuzzy search and update data
+      const results = fuse.search(searchTerm);
+      setData(results.map((result) => result.item));
+    }
+  }, [searchTerm]);
+
   return (
     <>
       <head>
@@ -25,6 +48,8 @@ export default function Dashboard() {
                   className="form-control"
                   name="email"
                   placeholder="Search for Anything..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
                 />
               </div>
             </div>
@@ -38,7 +63,7 @@ export default function Dashboard() {
           <br />
           <div className="row">
             {data.map((user) => (
-              <div className={`col-sm-4`}>
+              <div className={`col-sm-4`} key={user.id}>
                 <div className="card shadow my-2 p-3">
                   <img
                     className="card-img-top img-thumbnail"
@@ -52,7 +77,6 @@ export default function Dashboard() {
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
                     </p>
-                    {/*<span className="btn btn-secondary">Go somewhere</span>*/}
                   </div>
                 </div>
               </div>
